bdb678026155891cd4dca1fc97f89f19c0eec292
spff <spff@bitmessage.ch>
2018-03-10 00:27:19 +0800

    improve betterGuess algorithm, add document

--- app/src/main/java/tw/inspect/forchipright/MainFragment.kt
+++ app/src/main/java/tw/inspect/forchipright/MainFragment.kt
@@ -86,6 +86,7 @@ class MainFragment : Fragment(), AnkoLogger {
      *    from the candidateList depends on user's choice)
      *
      * 6.1. If betterGuess is chosen, determine whether to do in multi-thread, and how DEEP to guess
+     *      when there are multiple choices with the same VALUE(See the document below for detail)
      *
      * 7. Guess, and loop back to step 4.
      *
@@ -127,17 +128,80 @@ class MainFragment : Fragment(), AnkoLogger {
         }
 
 
-        fun betterGuess(matchList: List<Int>): Int {
+        /**
+         * Beside just choose the first from the candidateList, we calculate the Discrete Degree
+         * of each candidate in the candidateList.
+         *
+         * Take digits = 3 for example, every choice may get 0A0B 0A1B 0A2B 0A3B 1A0B 1A1B 1A2B 2A0B
+         * 2A1B and 3A0B, all 9 kinds of possible responses
+         *
+         * A+B less or equal 3
+         * -> A+B+LEFT = 3
+         * -> [ A ][ + ][ B ][ + ][ LEFT ] put 3 [1]s into [ A ] or [ B ] or [ LEFT ]
+         * -> 3*3 = 9
+         *
+         *
+         * The strategy is
+         *
+         * Whenever the Secret is, we should choose the one which may get the most
+         * Kinds of Different Possible Responses (Discrete Degree) so we may eliminate most
+         * impossible candidates from the next response.
+         *
+         * To do so, we're using a nested loop, the outer stands for Next Guess, the inner stands
+         * for if it is the Secret and what might the response according to Next Guess be. after
+         * doing statistic for all Next Guesses, we get the best Next Guess(es)
+         *
+         * Now digit = 2, the possible responses = 3*2 = 6, and pretend after the previous guess,
+         * after updating candidateList (via removing the impossible candidates), and we got
+         * 6 candidates left.
+         * We'd better choose the one, which Possible Kinds of Different Responses is 6, so after
+         * next response, we can remove the other candidates which might give either of the other 5
+         * responses according to the one we just chose to guess.
+         *
+         * More practical explanation
+         *
+         * we found candidate[3] will get response as shown below
+         *
+         * response           0A0B 0A1B 0A2B 1A0B 1A1B 2A0B
+         * count                1    1    1    1    1    1
+         * (index in candidate,                           (if we guess candidate[3] and get 2A0B
+         * for example)         0    1    2    4    5    3 the index of 2A0B should be 3)
+         *
+         * so if we guess candidate[3], and get a 0A2B response, after the candidateList updated,
+         * there will be only one candidate left, old candidate[2], and this should be the Secret.
+         *
+         *
+         * The second question is Which to choose between the below two situations and How
+         *
+         * case1
+         * response           0A0B 0A1B 0A2B 1A0B 1A1B 2A0B
+         * count                0    1    1    6    1    1
+         *
+         * case2
+         * response           0A0B 0A1B 0A2B 1A0B 1A1B 2A0B
+         * count                0    0    3    3    3    1
+         *
+         * It's the choice between average case and worst case.
+         * I go worst case, and I'm doing 1+1+36+1+1 > 9+9+9+1 , and I'll choose case2
+         *
+         *
+         * What if the value calculated the same
+         *
+         * Just pick the first, maybe we can do some calculate recursively, but not for now.
+         *
+         * */
 
-            var max = 0
-            val candidate = mutableListOf<Int>()
+        fun betterGuess(candidateList: List<Int>): Int {
 
-            matchList.forEach { pretendedResponse ->
+            var min = Int.MAX_VALUE
+            val bests = mutableListOf<Int>()
+
+            candidateList.forEach { nextGuess ->
                 mutableMapOf<Pair<Int, Int>, Int>().also { distributedMap ->
-                    matchList.forEach {
+                    candidateList.forEach {
                         var a = 0
                         var b = 0
-                        val list = pretendedResponse.toList()
+                        val list = nextGuess.toList()
                         for (i in 0 until digitCount) {
                             when {
                                 list[i] == it.ushr((digitCount - i - 1) * 4).and(15) -> a++
@@ -152,27 +216,27 @@ class MainFragment : Fragment(), AnkoLogger {
 
                     }
 
-                    info {distributedMap}
+                    //info {distributedMap}
 
-                }.values.filter { it > 0 }.size.apply {
-                    info { "$this $max $pretendedResponse" }
+                }.values.map { it * it }.reduce{a, b -> a+b}.apply {
+                    info { "$this $min $nextGuess" }
 
                     when{
-                        this > max -> {
-                            max = this
-                            candidate.clear()
-                            candidate.add(pretendedResponse)
+                        this < min -> {
+                            min = this
+                            bests.clear()
+                            bests.add(nextGuess)
                         }
-                        this == max -> candidate.add(pretendedResponse)
+                        this == min -> bests.add(nextGuess)
                     }
 
-                    info{ candidate }
+                    //info{ bests }
 
                 }
             }
 
             //return the first element which may lead to most kinds of result
-            return candidate[0]
+            return bests[0]
         }
 
 
@@ -279,7 +343,8 @@ class MainFragment : Fragment(), AnkoLogger {
         //takes about 16 sec for POOL_SIZE==10&&digitCount==9 as overallTime
         fun guessNumberLong() {
 
-            //the table stores all possible answers corresponding to results
+            //the table stores all possible Secrets
+            //and will be updated each time getting response.
             var candidateList = mutableListOf<Long>()
 
             fun genList(usableNumbers: List<Long>, current: Long) {
@@ -343,7 +408,8 @@ class MainFragment : Fragment(), AnkoLogger {
          * */
         fun guessNumber() {
 
-            //the table stores all possible answers corresponding to results
+            //the table stores all possible Secrets
+            //and will be updated each time getting response.
             var candidateList = mutableListOf<Int>()
 
             fun genList(usableNumbers: List<Int>, current: Int) {
