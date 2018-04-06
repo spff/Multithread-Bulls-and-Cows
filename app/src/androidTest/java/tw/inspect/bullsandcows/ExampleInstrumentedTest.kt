31b3067d8c5235e536932b56a77bcbfd03a8d2be
spff <spff@bitmessage.ch>
2018-04-06 20:51:20 +0800

    rename, remove dummy

--- app/src/androidTest/java/tw/inspect/bullsandcows/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/tw/inspect/bullsandcows/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("tw.inspect.forchipright", appContext.packageName)
+        assertEquals("tw.inspect.bullsandcows", appContext.packageName)
     }
 }
